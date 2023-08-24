bedddb15d2655a6c526e6d3323a192c1abb916f5
Kat Law <kathleen.law@multiverse.io>
2023-08-24 16:50:59 -0400

    add oauth and auth0, remove previous auth methods

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 require('dotenv').config()
 const express = require('express')
+const routes = require('./routes');
+const { auth } = require('express-openid-connect');
+
 const app = express()
 const PORT = 4000
-const routes = require('./routes');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:4000',
+  clientID: 'r6k6Qugzo6DmFAuSjjmwtkiE9WlexKzr',
+  issuerBaseURL: 'https://dev-kqcvt5qlx045drmf.us.auth0.com'
+};
+
+
 app.use(express.json())
+
+app.use(auth(config))
 
 app.use('/user', routes.user)
 app.use('/cupcakes', routes.cupcake);
