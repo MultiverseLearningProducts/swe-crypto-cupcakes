e53ad22479dc3cbb901fa05b32a3d0307facaa2a
davidalantodd <david.todd@multiverse.io>
2024-02-08 13:30:18 -0500

    edit GET /cupcakes/:id to keep original cupcakes array intact

--- routes/cupcake.js
+++ routes/cupcake.js
@@ -47,16 +47,16 @@ router.get('/', requiresAuth(), (req, res) => {
   res.json(decodedCupcakes)
 })
 
-// get a snippet by ID
+// get a cupcake by ID
 router.get('/:id', requiresAuth(), (req, res) => {
   const cupcakeId = parseInt(req.params.id)
-  const cupcake = cupcakes.find(cupcake => cupcake.id === cupcakeId)
+  let cupcake = cupcakes.find(cupcake => cupcake.id === cupcakeId)
 
   if (!cupcake) {
     return res.status(404).json({ error: 'Cupcake not found' })
   }
 
-  cupcake.instructions = decrypt(cupcake.instructions)
+  cupcake = {...cupcake, instructions: decrypt(cupcake.instructions)}
   res.json(cupcake)
 })
 
